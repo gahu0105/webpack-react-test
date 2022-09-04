7df0834e77ab76c7933690d567694620a3fdddc0
Han Seungnam <gahu0105@gmail.com>
2022-09-04 17:34:18 +0900

    feature-use-props

--- src/App.js
+++ src/App.js
@@ -9,35 +9,45 @@ import React from 'react';
 // );
 //사용자 정의 태그 => 컴포넌트
 // 반드시 대문자로 시작해야 한다.
-function Header(){
+function Header(props){
+    console.log('props', props);
     return <header>
-        <h1><a href="/">Web</a></h1>
+        <h1><a href="/">{props.title}</a></h1>
     </header>
 }
 
-function Nav(){
+function Nav(props){
+    const lis = []
+    for(let i=0; i<props.topics.length; i++)
+    {
+        let t = props.topics[i];
+        lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    }
     return  <nav>
                 <ol>
-                    <li><a href="/read/1">Html</a></li>
-                    <li><a href="/read/2">CSS</a></li>
-                    <li><a href="/read/3">Javascript</a></li>
+                    {lis}
                 </ol>
             </nav>
 }
 
-function Article(){
+function Article(props){
     return <article>
-        <h2>Welcome</h2>
-        Hello, Web
+        <h2>{props.title}</h2>
+        {props.body}
     </article>
 }
 
 function App() {
+    const topics = [
+        {id:1, title:'html', body:'html is ...'},
+        {id:2, title:'css', body:'css is ...'},
+        {id:3, title:'javascript', body:'javascript is ...'}
+    ]
     return (
         <div>
-            <Header/>
-            <Nav />
-            <Article />
+            <Header title="WEB"/>
+            <Nav topics = {topics}/>
+            <Article title="Welcome" body="Hello, Web!!"/>
         </div>
     );
 }
