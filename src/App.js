3e2950837fd7e39f65f2eb380a88a637fed2e7d9
Han Seungnam <gahu0105@gmail.com>
2022-09-04 17:11:29 +0900

    feature-create-component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,43 @@
 
 import React from 'react';
 
-const App = () =>(
-    <div>
-        Hello Webpack!!!
-    </div>
-);
+// const App = () =>(
+//     <div>
+//         Hello Webpack!!!
+//     </div>
+// );
+//사용자 정의 태그 => 컴포넌트
+// 반드시 대문자로 시작해야 한다.
+function Header(){
+    return <header>
+        <h1><a href="/">Web</a></h1>
+    </header>
+}
 
+function Nav(){
+    return  <nav>
+                <ol>
+                    <li><a href="/read/1">Html</a></li>
+                    <li><a href="/read/2">CSS</a></li>
+                    <li><a href="/read/3">Javascript</a></li>
+                </ol>
+            </nav>
+}
+
+function Article(){
+    return <article>
+        <h2>Welcome</h2>
+        Hello, Web
+    </article>
+}
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Nav />
+            <Article />
+        </div>
+    );
+}
 export default App;
