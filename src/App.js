e790c1e5b39909f32d99ffb8444d5fc00c0d7aa6
Han Seungnam <gahu0105@gmail.com>
2022-09-06 07:27:19 +0900

    'feature-use-create'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 
 import React from 'react';
+import './App.css';
 import {useState} from 'react';
 
 // const App = () =>(
@@ -11,9 +12,8 @@ import {useState} from 'react';
 //사용자 정의 태그 => 컴포넌트
 // 반드시 대문자로 시작해야 한다.
 function Header(props){
-    console.log('props', props);
     return <header>
-        <h1><a href="/" onClick={function(event){
+        <h1><a href="/" onClick={(event)=>{
             event.preventDefault(); //기본동작 방지, 클릭해도 Reload 안됨
             props.onChangeMode();
         }}>{props.title}</a></h1>
@@ -45,21 +45,39 @@ function Article(props){
     </article>
 }
 
+function Create(props){
+    return <article>
+        <h2>Create</h2>
+         <form onSubmit={event=>{
+            event.preventDefault();
+            const title = event.target.title.value;
+            const body = event.target.body.value;
+            props.onCreate(title, body);
+        }}>
+            <p><input type="text" name="title" placeholder="title"/></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit" value="Create"></input></p>
+        </form> 
+    </article>
+}
+
 function App() {
     // const _mode = useState("WELCOME");
     // const mode = _mode[0];
     // const setMode = _mode[1];
     const [mode, setMode] = useState("WELCOME");
     const [id, setID] = useState(null);
-    const topics = [
+    const [nextID, setNextID] = useState(4);
+    const [topics, setTopics] = useState([
         {id:1, title:'html', body:'html is ...'},
         {id:2, title:'css', body:'css is ...'},
         {id:3, title:'javascript', body:'javascript is ...'}
-    ]
+    ]);
     let content = null;
     if(mode === "WELCOME"){
         content = <Article title="Welcome" body = "Hello, WEB" />
-    }else if(mode === "READ"){
+    }
+    else if(mode === "READ"){
         let title, body = null;
         for(let i=0; i<topics.length; i++){
             console.log(topics[i].id, id);
@@ -70,6 +88,25 @@ function App() {
         }
         content = <Article title={title} body={body} ></Article>
     }
+    else if(mode === "CREATE")
+    {
+        content = <Create onCreate={(_title, _body)=>{
+            const newTopic = {id:nextID, title:_title, body:_body}
+            const newTopics = [...topics]
+            // primitive value (string, number, boolean등)
+            // Object type value (object, array)
+            // 복사 방법 
+            // primitive value : newValue = {...value}
+            // object value : newValue = [...value]
+            // console.log("nextID:",nextID);
+            newTopics.push(newTopic);
+            setTopics(newTopics);
+            setMode('READ');
+            setID(nextID);
+            setNextID(nextID+1);
+        }}></Create>
+    }
+
     return (
         <div>
             <Header title="WEB" onChangeMode={()=>{
@@ -78,9 +115,16 @@ function App() {
             <Nav topics = {topics} onChangeMode={(_id)=>{
                 setMode("READ");
                 setID(_id);
-            }}/>
+            }}></Nav>
             {content}
-            </div>
+
+            <ul>
+                <li><a href="/create" onClick={(event)=>{
+                    event.preventDefault();
+                    setMode("CREATE");
+                }}>Create</a></li>                
+            </ul>
+        </div>
     );
 }
 export default App;
